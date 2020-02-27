d3edffefcc30b4998541de0b279cfa703bba3cf4
jiandandaoxingfu <jiaminxin@outlook.com>
2020-02-27 09:55:38 +0800

    Updates

--- ArmLink.js
+++ ArmLink.js
@@ -1,19 +1,19 @@
 class ArmLink {
-	// 正向运动学(forward kinematics)，绘制机械臂。
 	// 考虑三自由度机械臂。
 	constructor(canvas, link_length) {
 		this.canvas = canvas;
 		this.link_length = link_length;
 		this.arm_joint_arr = [];
 		this.joint_angles = [];
-		this.isRender = true;
+		this.is_render = true;
 	}
 
 	update_joint() {
+		// 正向运动学(forward kinematics)，绘制机械臂。
 		let arm_joint_arr = [[0, 0]];
 		for(let i=0; i<this.joint_angles.length; i++) {
 			let angle = this.joint_angles.slice(0, i+1).reduce( (i, j) => i+j );
-			arm_joint_arr[i+1] = vector2.transform([this.link_length[i], 0], angle, arm_joint_arr[i]);
+			arm_joint_arr[i+1] = this.transform([this.link_length[i], 0], angle, arm_joint_arr[i]);
 		}
 		this.arm_joint_arr = arm_joint_arr;
 	}
@@ -21,77 +21,89 @@ class ArmLink {
 	plot() {
 		let points = '', circle = '';
 		this.arm_joint_arr.forEach( p => {
-			circle += `<circle cx="${p[0] + 300}" cy="${p[1] + 300}" r="3" style="stroke-width: 1;  fill: black;"/>`;
+			circle += `<circle cx="${p[0] + 300}" cy="${p[1] + 300}" r="3" style="stroke-width: 2;  fill: black;"/>`;
 			points += (p[0] + 300) + ',' + (p[1] + 300) + ' ';
 		})
-		document.querySelector('#canvas').innerHTML = `<polyline points="${points}" style="fill: none; stroke:red; stroke-width:1"/>` + circle;
+		document.querySelector('#arm').innerHTML = `<polyline points="${points}" style="fill: none; stroke:red; stroke-width:3"/>` + circle;
 	}
 
 	get_joint_angle(x, y) {
+		// 逆向动力学，求夹角。
 		let lens = this.link_length;
-		let theta123, phi, theta12, psi, theta1, theta2, theta3;
+		let theta123, phi, theta12, psi, theta1, theta1_, theta2, theta3;
 		phi = theta123 = atan2(y, x);
 		let a = 2 * (x -  lens[2] * cos(phi)) * lens[1];
 		let b = 2 * (y -  lens[2] * sin(phi)) * lens[1];
 		let c = x*x + y*y + lens[1]*lens[1] + lens[2]*lens[2] - lens[0]*lens[0] - 2*lens[2]*( x*cos(phi) + y*sin(phi) );
 		let d = b*b + a*a - c*c;
 		if( d < 0 ) {
-			console.log('无法达到');
+			this.canvas.innerHTML +=`<circle cx="${x + 300}" cy="${y + 300}" r="2" style="stroke-width: 0; fill: gray;"/>`;
 			return false;
 		} else {
-			psi = theta12 = 2*atan( (b - sqrt(d) ) / (a + c) );
+			psi = theta12 = 2*atan( (b + sqrt(d) ) / (a + c) ); // b +/- sqrt(d)都可以。
 			theta1 = acos( (x - lens[2]*cos(phi) - lens[1]*cos(psi)) / lens[0] );
+			theta1_ = asin( (y - lens[2]*sin(phi) - lens[1]*sin(psi)) / lens[0] )
 			theta2 = theta12 - theta1;
 			theta3 = theta123 - theta12;
-			console.log( [theta1, theta2, theta3, theta12, theta123].map( a => (a*180/pi).toFixed(3)) );
-			if( abs(theta1*180/pi - 180) < 1 ) {
-				console.log('theta1临界值');
+			if( theta1_ < 0 ) {
 				theta1 = -theta1;
-			}
-			if( abs(theta1*180/pi) < 1 ) {
-				console.log('theta1临界值');
-			}
-			if( abs(theta2*180/pi + 180) < 1 ) {
-				console.log('theta2临界值');
-			}
-			if( abs(theta3*180/pi + 180) < 1 ) {
-				console.log('theta3临界值');
+				theta2 = theta12 - theta1;
 			}
 			this.joint_angles = [theta1, theta2, theta3];
-			// console.log('123, 12, 1: ' + [theta123, theta12, theta1].map( a => a*180/pi ));
-			// console.log('1, 2, 3: ' + [theta1, theta2, theta3].map( a => a*180/pi ));
 			return true;
 		}
 	}
 
 	render(x, y) {
-		this.isRender = !1;
+		this.is_render = !1; // 控制更新频率
 		setTimeout(() => {
-			this.isRender = !0;
-		}, 100);
+			this.is_render = !0;
+		}, 50);
 		if( this.get_joint_angle(x, y) ) {
 			this.update_joint();
 			this.plot();	
 		}
 	}
 
-	click() {
+	mousemove() {
 		document.addEventListener('mousemove', e => {
 			let ele = e.target;
 			if( ele.id && ele.id === 'canvas' ) {
 				let box = this.canvas.getBoundingClientRect()
-				let x = Math.round(e.clientX - box.left) - 300;
-				let y = 300 - Math.round(e.clientY - box.top);
-				if( this.isRender ) {
-					console.log([x, y]);
+				let x = round(e.clientX - box.left) - 300;
+				let y = 300 - round(e.clientY - box.top);
+				if( this.is_render ) {
+				
 					this.render(x, y);
+				
 				}
 			}
 		})
 	}
+
+	transform(OA, alpha, translate) {
+		let mat = [
+			[cos(alpha), -sin(alpha)],
+			[sin(alpha), cos(alpha)]
+		];
+		return [mat[0][0] * OA[0] + mat[0][1] * OA[1] + translate[0], mat[1][0] * OA[0] + mat[1][1] * OA[1] + translate[1]];
+	}
 }
 
+var sqrt = Math.sqrt;
+var sin = Math.sin;
+var asin = Math.asin;
+var atan = Math.atan;
+var atan2 = Math.atan2;
+var cos = Math.cos;
+var acos = Math.acos;
+var round = Math.round;
+var random = Math.random;
+const pi = Math.PI;
 
-var armLink = new ArmLink(document.querySelector('#canvas'), [100, 100, 100]);
-armLink.click();
-armLink.render(300, 0);
+var link_length = [70 + round(30*random()), 70 + round(30*random()), 70 + round(30*random())];
+var length = link_length.reduce( (i, j) => i+j);
+var armLink = new ArmLink(document.querySelector('#canvas'), link_length);
+armLink.canvas.innerHTML += `<circle cx="300" cy="300" r="${length}" style="stroke-width: 2; stroke: black; fill: none;"/>`;
+armLink.mousemove();
+armLink.render(length, 0);
---
+++ README.md
@@ -0,0 +1,39 @@
+# 三自由度机械臂
+
+## 算法
+关键在于DH-table的引入。
+
+![图片无法加载](images/示意图.jpg)
+
+设终点坐标(相对于x_0-y_0坐标系)为(x, y)。利用正向运动学，可得如下关系
+
+![图片无法加载](images/eq1.PNG)
+
+以及
+
+![图片无法加载](images/eq2.PNG)
+
+其中
+
+![图片无法加载](images/eq3.PNG)
+
+由此可得
+
+![图片无法加载](images/eq4.PNG)
+
+其中
+
+![图片无法加载](images/eq5.PNG)
+
+由以上结果就可以得到theta_1, theta_2, theta_3。然后利用正向运动学即可得到节点处的坐标，从而绘制出图形。
+
+## Demo
+
+机械臂跟随鼠标当前位置，[demo](https://jiandandaoxingfu.github.io/point-tracking)。
+
+## 参考资料 
+
+- 知乎专栏：[何小白：机器人学学习笔记](https://zhuanlan.zhihu.com/c_1208050340920299520).
+
+## 问题
+- 当theta1， theta2达到临界值(180, 0, -180)时，无法正确跟踪目标点。
